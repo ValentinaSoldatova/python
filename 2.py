468242c826930809c0ccd2de139b063d6951e67d
ValentinaSoldatova <vs08@inbox.ru>
2021-10-10 00:15:28 +0500

    Initial commit

---
+++ 2.py
@@ -0,0 +1,8 @@
+
+seconds = int(input("Введите время в секундах: "))
+print (seconds)
+
+minutes = seconds / 60
+hours = minutes /60
+
+print (f"введенное число это {hours}часов, {minutes}минут, {seconds}секунд")
